4c8ef9f0134ea1e67e05bc8fc5b6e19cf0915199
Tomas Guardo <guardotomas02@gmail.com>
2021-02-17 14:19:33 -0300

    Update LogIn.js

--- routes/LogIn.js
+++ routes/LogIn.js
@@ -4,18 +4,28 @@ const Mongoose = require("mongoose");
 const User = require("../schema/User")
 const LogIn = Express();
 
-
-
+// Request username from server and if user exists and password matches, 
+// then return user 
 LogIn.get("/login", async (req, res) => {
-    let user;
-    try{
-        const user = await User.findOne({username: "" + req.body.username})
-        if((!user) | (user.password != req.body.password)) throw new Error("Invalid user or password");
-        res.json(user);
-    }
-    catch(err){
-        res.json({message: err.message});
-    }
+    if(req.body.username){
+        try{
+            const user = await User.findOne({username: "" + req.body.username}, {__v: false})
+            if((!user) || (user.password != req.body.password)) throw new Error("Invalid user or password");
+            res.json(user);
+        }
+        catch(err){
+            res.json({message: err.message});
+        }
+    } else 
+        if(!!req.body.id){
+            try{
+                const user = await User.findById(req.body.id);
+                if(!user) throw new Error("Token expired");
+                res.json(user);
+            }catch(err){
+                res.json({message: err.message});
+            } 
+        }
 })
 
 module.exports = LogIn; 
