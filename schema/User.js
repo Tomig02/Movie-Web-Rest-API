14661c65697618a0bf6bd4cd1559aa58f451a384
Tomas Guardo <guardotomas02@gmail.com>
2021-02-21 16:25:51 -0300

    Update User.js

--- schema/User.js
+++ schema/User.js
@@ -3,10 +3,13 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
     username: String,
     password: String,
+    email: String,
     name: String,
     surname: String,
     age: String,
-    gender: String
+    gender: String,
+    favoriteMovies: [],
+    savedMovies: []
 },{collection: "Users"})
 
 module.exports = mongoose.model("User", UserSchema);
